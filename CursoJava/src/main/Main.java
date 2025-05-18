11f0205827f048c36b4fda80ba13877934c45b75
AsherGarciaO <asherg.o@outlook.com>
2025-05-17 20:03:41 -0600

    Modulo1

---
+++ CursoJava/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import operaciones.CambiosBase;
+
+public class Main {    
+    public static void main(String[] args) {
+       CambiosBase.binDec("1010");
+       CambiosBase.decBin(10);
+    }
+}
---
+++ CursoJava/src/operaciones/CambiosBase.java
@@ -0,0 +1,19 @@
+package operaciones;
+
+public class CambiosBase {    
+    public static String decBin(int numero){
+        String binario = "";
+        
+        //Codificar Un Algoritmo de su Gusto para convertir decimal a Binario
+        
+        return binario;
+    }
+    
+    public static int binDec(String binario){
+        int decimal = 0;
+        
+        //Codificar Un Algoritmo de su Gusto para convertir binario a Decimal
+        
+        return decimal;
+    }
+}
